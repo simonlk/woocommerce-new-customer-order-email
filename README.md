3f65296d392e48df348bb7f889d788d8cde2bfbc
Simon Kelly <simon@renegade-empire.com>
2017-01-28 17:28:33 +1100

    Initial commit. Forked from https://www.skyverge.com/blog/how-to-add-a-custom-woocommerce-email/

---
+++ README.md
@@ -0,0 +1,4 @@
+woocommerce-processing-completed-order-email
+=================================
+
+Plugin to send the customer an email when their order is processing or complete. 
---
+++ woocommerce-new-customer-order-email.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: WooCommerce New Customer Order Email
+ * Plugin URI: 
+ * Description: 
+ * Author: 
+ * Author URI: 
+ * Version: 0.1
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ *  Add a custom email to the list of WooCommerce emails
+ *
+ * @since 0.1
+ * @param array $email_classes available email classes
+ * @return array filtered available email classes
+ */
+function add_woocommerce_new_customer_order_email( $email_classes ) {
+
+	// include our custom email class
+	require_once( 'includes/class-woocommerce-new-customer-order-email.php' );
+
+	// add the email class to the list of email classes that WooCommerce loads
+	$email_classes['WC_New_Customer_Order_Email'] = new WC_New_Customer_Order_Email();
+
+	return $email_classes;
+
+}
+add_filter( 'woocommerce_email_classes', 'add_woocommerce_new_customer_order_email' );
